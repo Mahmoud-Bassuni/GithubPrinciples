7eb03ac2150f21e4e75fdcac1dd5157235830d48
Mahmoud Basyouni <mbasyouny@halalah.sa>
2020-03-21 23:04:47 +0200

    create the project

---
+++ GithubPrinciples/GithubPrinciples/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GithubPrinciples
+//
+//  Created by Mahmoud Basyouny on 3/21/20.
+//  Copyright © 2020 Mahmoud Basyouny. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
