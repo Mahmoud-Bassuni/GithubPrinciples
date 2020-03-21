02fa30194f1fab8d5679f15c1e7e399b9a2afb00
Mohammed ElSedody <mohammed.elsdody@egabi.com>
2020-03-22 00:15:38 +0200

    second commit

--- GithubPrinciples/GithubPrinciples/ViewController.swift
+++ GithubPrinciples/GithubPrinciples/ViewController.swift
@@ -14,7 +14,8 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-    override func viewDidAppear(_ animated: Bool) {
+    override func viewWillAppear(_ animated: Bool) {
+        
          // Do any additional setup after loading the view.
         let s = "sfsfsfsf"
     }
